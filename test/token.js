1a0d6064bb077ba06af96bf7cb35058a9c5e1de1
Nexus-Agni <agnibhachakraborty11@gmail.com>
2024-08-08 18:28:36 +0530

    Add token transfer functionality between accounts

--- test/token.js
+++ test/token.js
@@ -13,7 +13,7 @@ describe ("Token", function () {
     })
 
     it("Should transfer tokens between accounts", async function () {
-        const [owner, addr1] = await ethers.getSigners();
+        const [owner, addr1, addr2] = await ethers.getSigners();
         const Token = await ethers.getContractFactory("Token");
         const hhtoken = await Token.deploy();
 
@@ -23,6 +23,13 @@ describe ("Token", function () {
 
         expect(bal1).to.equal(900);
         expect(bal2).to.equal(100);
+
+        await hhtoken.connect(addr1).transfer(addr2.address, 30)
+        const bal3 = await hhtoken.balanceOf(addr1.address);
+        const bal4 = await hhtoken.balanceOf(addr2.address);
+
+        expect(bal3).to.equal(70);
+        expect(bal4).to.equal(30);
     })
 
 })
